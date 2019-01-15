efbb82cd85f00897d80871e4fdb6a4cdc2e1e4cb
ROLLIER <rollier@min.fr>
2019-01-15 16:16:33 +0100

    Boostrap java project

---
+++ src/main/java/esiea/archlog/App.java
@@ -0,0 +1,13 @@
+package esiea.archlog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
